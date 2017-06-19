092937d720d782676d98ca19a1a1f52616aa7c0d
Max Scher and Rena Rallis <max@>
2017-06-19 15:52:22 -0700

    fix merge errors with status panel and container

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,7 @@ var radioInput1 = $("input:radio[name=dec1radio]:checked").val();
     showExplanationd12();
   };
 };
+
 Candidate.prototype.taxesDec = function(){
 var radioInput2 = $("input:radio[name=dec2radio]:checked").val();
   if (radioInput2 == "cuttax") {
@@ -47,6 +48,7 @@ var radioInput2 = $("input:radio[name=dec2radio]:checked").val();
     showExplanationd23();
   };
 };
+
 Candidate.prototype.attackDec = function(){
 var radioInput3 = $("input:radio[name=dec3radio]:checked").val();
   if (radioInput3 == "attack") {
@@ -65,6 +67,7 @@ var radioInput3 = $("input:radio[name=dec3radio]:checked").val();
     showExplanationd33();
   };
 };
+
 Candidate.prototype.winLose = function() {
   if (this.reputation >= 9) {
     victoryReveal();
@@ -117,6 +120,7 @@ $(document).ready(function() {
   showExplanationd33 = function() {
     $("#explanationd3-3").show()
   };
+
   $("#register").submit(function(event) {
     event.preventDefault();
     var firstName = $("input#first-name").val();
@@ -139,28 +143,34 @@ $(document).ready(function() {
     $("button#decision1Button").hide();
     $("button#moveon1Button").show();
   });
+
   $("button#moveon1Button").click(function(){
     $("#decision1").hide();
     $("#decision2").show();
   });
+
   $("button#decision2Button").click(function(){
     newCandidate.taxesDec();
     $("button#decision2Button").hide();
     $("button#moveon2Button").show();
   });
+
   $("button#moveon2Button").click(function(){
     $("#decision2").hide();
     $("#decision3").show();
   });
+
   $("button#decision3Button").click(function(){
     newCandidate.attackDec();
     $("button#decision3Button").hide();
     $("button#moveon3Button").show();
   });
+
   $("button#moveon3Button").click(function(){
     $("#decision3").hide();
     newCandidate.winLose();
   });
+
   $("#final-decision-button").submit(function(event) {
 
   });
