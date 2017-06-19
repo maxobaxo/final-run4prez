3b3e281ad15ded9299e2cb8d15acd9dee2aa8667
Max Scher, Nathan Stewart, Rena Rallis and Steve Spitz <steve@>
2017-06-19 09:45:01 -0700

    add basic constructor function for Candidate object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+//business logic
+function Candidate(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.reputation = 0;
+  this.funding = 0;
+}
+
+//user interface logic
 $(document).ready(function() {
-  
+  $("#register").submit(function(event) {
+    event.preventDefault();
+
+    var newCandidate = new Candidate(name, age, gender);
+  });
 });
