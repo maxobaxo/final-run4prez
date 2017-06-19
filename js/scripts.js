acfbdd5a8bb5dca0d426d8f260e09c2a08626297
Max Scher and Rena Rallis <max@>
2017-06-19 14:59:04 -0700

    restyle scoreboard with status panel

--- js/scripts.js
+++ js/scripts.js
@@ -19,15 +19,11 @@ var radioInput1 = $("input:radio[name=dec1radio]:checked").val();
   if (radioInput1 == "environmental") {
     this.reputation = this.reputation + 5;
     this.funding = this.funding + 500;
-    console.log("enviro trigger")
-    console.log(this)
   } else if (radioInput1 == "industrial") {
     this.reputation = this.reputation + 2;
     this.funding = this.funding + 1000;
-    console.log("industry trigger")
-    console.log(this)
   } else {
-    console.log("conditions have failed")
+
   }
   $("#decision1").hide()
   $("#decision2").show()
@@ -63,8 +59,6 @@ $(document).ready(function() {
     $(".funds").append(newCandidate.funding);
     $("#status").show();
     $("#decision1").show();
-
-    console.log(newCandidate.reputation);
   });
 
   $("button#decision1Button").click(function(){
